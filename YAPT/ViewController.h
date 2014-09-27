4d26cf03dd4f16c420a6b62a734dfffc735b0a58
Bill Glover <bill@billglover.co.uk>
2014-09-27 14:38:53 +0100

    Toggle pomodoro status based on button press

--- YAPT/ViewController.h
+++ YAPT/ViewController.h
@@ -7,9 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "YAPTPomodoro.h"
 
 @interface ViewController : UIViewController
 
-
 @end
-
--- YAPT/ViewController.m
+++ YAPT/ViewController.m
@@ -12,6 +12,7 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet RoundButton *progressButton;
 @property (weak, nonatomic) IBOutlet UISlider *demoSlider;
+@property (nonatomic, strong, readwrite) YAPTPomodoro *currentPomodoro;
 @end
 
 @implementation ViewController
@@ -28,11 +29,43 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - User Interaction
+
 - (IBAction)progressButtonPress:(RoundButton *)sender {
-    NSLog(@"progressButtonPress");
+    
+    if (self.currentPomodoro.state == pomodoroNewState) {
+        NSLog(@"Starting a new pomodoro");
+        [self.currentPomodoro startPomodoro];
+        
+        // update the button label
+        [self.progressButton setTitle:@"Abort" forState:UIControlStateNormal];
+    }
+    
+    else if (self.currentPomodoro.state == pomodoroActiveState) {
+        NSLog(@"Voiding the current pomodoro");
+        [self.currentPomodoro voidPomodoro];
+        
+        // discard the pomodoro for now
+        self.currentPomodoro = nil;
+        
+        // update the button label
+        [self.progressButton setTitle:@"Start" forState:UIControlStateNormal];
+
+    }
+    
 }
 
 - (IBAction)demoSliderChanged:(UISlider *)sender {
     self.progressButton.percent = self.demoSlider.value;
 }
+
+#pragma mark - Getters & Setters
+
+- (YAPTPomodoro *)currentPomodoro {
+    if (!_currentPomodoro) {
+        _currentPomodoro = [[YAPTPomodoro alloc] init];
+    }
+    return _currentPomodoro;
+}
+
 @end
